45aedb5f572ecd04d6b2c8fd18154c41228ff179
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-28 23:35:36 +0000

    changes

--- opcode_functions_1.c
+++ opcode_functions_1.c
@@ -1,10 +1,11 @@
 #include "monty.h"
 
 /**
- * push - add a node to the start of a doubly linked list
+ * push_op - add a node to the start of a doubly linked list
  *
  * @head: list head
  * @n: integer data
+ * @line_number: the line of access
  *
  * Return: 0
  */
@@ -21,9 +22,10 @@ void push_op(stack_t **head, int n, unsigned int line_number)
 }
 
 /**
- * pall - prints a doubly linked list
+ * pall_op - prints a doubly linked list
  *
- * @h: list head
+ * @head: list head
+ * @line_number: the line of access
  *
  * Return: void
  */
@@ -40,21 +42,78 @@ void pall_op(stack_t **head, unsigned int line_number)
 		node = node->next;
 	}
 }
-
+/**
+ * pint_op - prints first element of doubly linked list
+ *
+ * @head: list head
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void pint_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	if (*head == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	while ((*head)->prev != NULL)
+		(*head) = (*head)->prev;
+	printf("%d\n", (*head)->n);
 }
-
+/**
+ * pop_op - removes top element of a doubly linked list
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void pop_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
-}
+	stack_t *temp;
 
+	if (*head == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *head;
+	while (temp->prev != NULL)
+		temp = temp->prev;
+	(*head) = (*head)->next;
+	(*head)->prev = NULL;
+	free(temp);
+}
+/**
+ * swap_op - swaps top two elements of doubly linked list
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void swap_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	stack_t *temp, *hold;
+	int holder;
+
+	if ((*head) != NULL || (*head)->prev != NULL)
+	{
+		while ((*head)->prev != NULL)
+			(*head) = (*head)->prev;
+		temp = *head;
+		hold = temp->next;
+		holder = temp->n;
+		temp->n = hold->n;
+		hold->n = holder;
+	}
+	else
+	{
+		printf("L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
--- opcode_functions_2.c
+++ opcode_functions_2.c
@@ -1,21 +1,75 @@
 #include "monty.h"
-
+/**
+ * add_op - add two elements of doubly linked list together
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void add_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	stack_t *temp;
+
+	temp = *head;
+	if ((*head) != NULL || (*head)->next != NULL)
+	{
+		while ((*head)->prev != NULL)
+			(*head) = (*head)->prev;
+		(*head) = (*head)->next;
+		(*head)->n = temp->n + (*head)->n;
+		free(temp);
+	}
+	else
+	{
+		printf("L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
+/**
+ * nop_op - a function that does nothing
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 
 void nop_op(stack_t **head, unsigned int line_number)
 {
 	(void) head;
 	(void) line_number;
+	return;
 }
-
+/**
+ * sub_op - subtract first two elements of doubly linked list
+ *
+ * @head: the list
+ * @line_number: line of access
+ *
+ * Return: void
+ *
+ */
 void sub_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	stack_t *temp;
+
+	temp = *head;
+	if ((*head) != NULL || (*head)->next != NULL)
+	{
+		while ((*head)->prev != NULL)
+			(*head) = (*head)->prev;
+		(*head) = (*head)->next;
+		(*head)->n = (*head)->n - temp->n;
+		free(temp);
+	}
+	else
+	{
+		printf("L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void div_op(stack_t **head, unsigned int line_number)
