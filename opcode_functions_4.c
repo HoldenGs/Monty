7008dfa1ba88d4f6bdbb4635adbcdc3f1c98f8c9
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-29 23:57:33 +0000

    rotl

--- opcode_functions_4.c
+++ opcode_functions_4.c
@@ -9,8 +9,22 @@
  */
 void rotl_op(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-	(void) line_number;
+	stack_t *temp, *hold;
+
+	temp = *head;
+	if (temp == NULL)
+		return;
+	while (temp->next != NULL)
+		temp = temp->next;
+	hold = malloc(sizeof(stack_t));
+	if (hold == NULL)
+		return;
+	hold->n = (*head)->n;
+	hold->next = NULL;
+	hold->prev = temp;
+	temp->next = hold;
+	pop_op(head, line_number);
+
 }
 /**
  * rotr_op - rotates stack to the bottom
