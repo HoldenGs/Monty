9e76f5ce29d4c90b974d3a0417f523714caad76d
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-29 21:01:50 +0000

    woops, wrong input

--- opcode_functions_2.c
+++ opcode_functions_2.c
@@ -90,7 +90,7 @@ void div_op(stack_t **head, unsigned int line_number)
 	}
 	if ((*head)->n == 0)
 	{
-		printf("L%d: division by zero\n", temp->n);
+		printf("L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = *head;
